9c6181f7d98d8a2306f6c2af6e68863841f0e3b9
dobri1408 <dobriceanionut1408@gmail.com>
2024-12-26 14:02:50 +0200

    d

--- classes/template.class.php
+++ classes/template.class.php
@@ -2,76 +2,34 @@
 
 class appTemplate {
     
-    /*
-    The filename of the template to load from views/
-    */
     protected $file;
-
-    /*
-    An array of values for replacing each tag on the template
-    */
     protected $values = array();
 
-    /*
-    Creates a new Template object and sets its associated file.
-    */
     public function __construct($file) {
         $this->file = APPLICATION_PATH . DS . "views" . DS . $file;
     }
     
-    public function init()
-    {
-        $baseUrl = appTemplate::getBaseUrl();
+    public function init() {
+        $baseUrl = self::getBaseUrl();
         $this->set("baseUrl", $baseUrl);
     }
     
-    public static function getBaseUrl()
-    {
-        // Determine if HTTPS is being used
+    public static function getBaseUrl() {
         $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https" : "http";
-
-        // Get host and current script path
         $host = $_SERVER['HTTP_HOST'];
         $scriptPath = dirname($_SERVER['PHP_SELF']);
-
-        // Build the base URL
-        $baseUrl = $protocol . "://" . $host . $scriptPath;
-
-        // Remove trailing slash if present
-        return rtrim($baseUrl, '/');
+        return rtrim($protocol . "://" . $host . $scriptPath, '/');
     }
 
-    /*
-    Sets a value for replacing a specific tag.
-    */
     public function set($key, $value) {
         $this->values[$key] = $value;
     }
 
-    public static function refreshSessionState()
-    {
-        // Update session-dependent state dynamically
-        if (!empty($_SESSION['type_account']) && $_SESSION['type_account'] === 'admin') {
-            $_SESSION['admin_menu'] = '<li><a href="[@baseUrl]/add/">Submit News</a></li>';
-        } else {
-            $_SESSION['admin_menu'] = '';
-        }
-    }
-
-    public static function clearSessionState()
-    {
-        // Reset session-dependent variables
-        unset($_SESSION['admin_menu']);
-    }
-
-    /*
-    Outputs the content of the template, replacing the keys for its respective values.
-    */
-    public function output()
-    {
+    public function output() {
         if (!file_exists($this->file)) {
             return "Error loading template file ($this->file).<br />";
         }
+
         $output = file_get_contents($this->file);
 
         foreach ($this->values as $key => $value) {
@@ -85,46 +43,28 @@ class appTemplate {
 
         return $output;
     }
-    
-    /*
-    Merges the content from an array of templates and separates it with $separator.
-    */
-    static public function merge($templates, $separator = "\n") {
 
-        $output = "";
-
-        foreach ($templates as $template) {
-            $content = (get_class($template) !== "appTemplate")
-                ? "Error, incorrect type - expected appTemplate."
-                : $template->output();
-            $output .= $content . $separator;
+    public static function refreshSessionState() {
+        if (!empty($_SESSION['type_account']) && $_SESSION['type_account'] === 'admin') {
+            $_SESSION['admin_menu'] = '<li><a href="' . self::getBaseUrl() . '/add/">Submit News</a></li>';
+        } else {
+            $_SESSION['admin_menu'] = '';
         }
-        
-        return $output;
     }
-    
-    /*
-    Redirect the page to $url
-    */
-    public static function redirect($url)
-    {
-        header("Location: $url");
-        exit;
+
+    public static function clearSessionState() {
+        unset($_SESSION['admin_menu']);
     }
-    
-    /*
-    Load main layout
-    */
-    public static function loadLayout($params = array())
-    {
-        $layout = new appTemplate("layout.phtml");
-        
+
+    public static function loadLayout($params = array()) {
+        $layout = new self("layout.phtml");
+
         if (isset($_SESSION['msg'])) {
             $layout->set("message", $_SESSION['msg']);
             $layout->set("message_class", "extra");
             unset($_SESSION['msg']);
         }
-        
+
         if (is_array($params)) {
             foreach ($params as $key => $value) {
                 $layout->set($key, $value);
@@ -134,17 +74,5 @@ class appTemplate {
         $layout->init();
 
         return preg_replace("/\[@(.*)\]/", "", $layout->output());
-    }
-
-    /*
-    Determines if the user is an admin and returns the admin menu HTML
-    */
-    private function getAdminMenu()
-    {
-        // Check for admin rights dynamically
-        if (!empty($_SESSION['type_account']) && $_SESSION['type_account'] === 'admin' && !empty($_SESSION['logged_in']) && $_SESSION['logged_in']) {
-            return '<li><a href="[@baseUrl]/add/">Submit News</a></li>';
-        }
-        return '';
     }
 }
--- controllers/news.php
+++ controllers/news.php
@@ -1,20 +1,15 @@
 <?php
 
 class appController {
-    
-    public static function indexAction($args = array())
-    {
-        self::checkAuthentication(); // Verifică dacă utilizatorul este autentificat
+
+    public static function indexAction($args = array()) {
+        self::checkAuthentication();
 
         $db = new DB();
-        $query = "";
-        if (isset($args[0])) {
-            $query = App::test_input(strip_tags($args[0]));
-        }
-        
+        $query = isset($args[0]) ? App::test_input(strip_tags($args[0])) : "";
         $news = $db->getNewsByQuery($query);
         $list_output = "";
-        
+
         if (is_array($news)) {
             foreach ($news as $post) {
                 if (!$post["permalink"]) {
@@ -33,109 +28,24 @@ class appController {
         $view = new appTemplate("news/index.phtml");
         $view->set("title", "Latest news");
         $view->set("content", $list_output);
-        
+
         return appTemplate::loadLayout(array("content" => $view->output(), "title" => "Homepage", "query" => stripslashes($query)));
     }
-    
-    public static function searchNewsAction($args = array())
-    {
-        self::checkAuthentication();
-        $query = $_GET["query"];
-        return appController::indexAction(array($query));
-    }
-    
-    public static function addNewsAction($args = array(), $update_news = 0)
-    {
-        self::checkAuthentication();
-        $db = new DB();
-        $news = array("title" => "", "author" => "", "date" => "", "time" => "", "content" => "", "updated" => "", "id" => "");
-        $news_id = 0;
-        
-        if ($args && $args[0]) {
-            $news_id = (int) $args[0];
-            $news = $db->loadNews($news_id);  
-            if (!$news) {
-                appTemplate::redirect(appTemplate::getBaseUrl());
-            }
-            $news["date"] = date("Y-m-d", strtotime($news["created"]));
-            $news["time"] = date("H:i", strtotime($news["created"]));
-        }
-        
-        if ($_SERVER['REQUEST_METHOD'] == "POST") {
-            $news_id = $db->saveNews($news_id);
-            appTemplate::redirect(appTemplate::getBaseUrl());
-        }
-        
-        $view = new appTemplate("news/add.phtml");
-        $view->set("pageTitle", $news_id ? "Edit News #" . $news['id'] : "Add News");
-        $view->set("news_id", $news['id']);
-        $view->set("input_title", $news['title']);
-        $view->set("input_author", $news['author']);
-        $view->set("input_date", $news['date']);
-        $view->set("input_time", $news['time']);
-        $view->set("input_content", $news['content']);
-        
-        return appTemplate::loadLayout(array("content" => $view->output(), "title" => $news_id ? "Edit News" : "Add News"));
-    }
-    
-    public static function updateNewsAction($args = array())
-    {
-        self::checkAuthentication();
-        return appController::addNewsAction($args, 1);
-    }
-    
-    public static function viewNewsAction($args = array())
-    {
-        self::checkAuthentication();
-        $db = new DB();
-        $news_id = $args[0];
-        
-        if (!is_numeric($news_id)) {
-            $post = $db->getNewsByPermalink($news_id);
-        } else {
-            $post = $db->loadNews($news_id);
-        }
-        
-        if (!$post) {
-            appTemplate::redirect(appTemplate::getBaseUrl());
-        }
-        
-        $view = new appTemplate("news/view.phtml");
-        $view->set("title", $post["title"]);
-        $view->set("author", $post["author"]);
-        $view->set("date", date("M d, Y H:i", strtotime($post["created"])));
-        $view->set("content", htmlspecialchars_decode($post["content"]));
-        $view->set("news_id", $post["id"]);
-        
-        return appTemplate::loadLayout(array("content" => $view->output(), "title" => $post["title"]));
-    }
-    
-    public static function deleteNewsAction($args = array())
-    {
-        self::checkAuthentication();
-        $news_id = (int) $args[0];
-        $db = new DB();
-        $db->deleteNews($news_id);
-        appTemplate::redirect(appTemplate::getBaseUrl());
-    }
-    
-    public static function loginAction($args = array())
-    {
+
+    public static function loginAction($args = array()) {
         $error = "";
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $username = App::test_input($_POST['username']);
             $password = App::test_input($_POST['password']);
-
             $db = new DB();
             $user = $db->getUserByUsername($username);
 
             if ($user && password_verify($password, $user['password_hash'])) {
                 $_SESSION['logged_in'] = true;
                 $_SESSION['username'] = $username;
-                $_SESSION['username'] = $user['type_account'];
-                $error = "wrf";
-                appTemplate::refreshSessionState();
+                $_SESSION['type_account'] = $user['type_account'];
 
+                appTemplate::refreshSessionState();
                 appTemplate::redirect(appTemplate::getBaseUrl());
             } else {
                 $error = "Invalid username or password!";
@@ -148,82 +58,13 @@ class appController {
         return appTemplate::loadLayout(array("content" => $view->output(), "title" => "Login"));
     }
 
-    public static function registerEditorAction($args = array())
-    {
-        $error = "";
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $username = App::test_input($_POST['username']);
-            
-            $email = App::test_input($_POST['email']);
-            $password = App::test_input($_POST['password']);
-            $confirm_password = App::test_input($_POST['confirm_password']);
-
-            if ($password !== $confirm_password) {
-                $error = "Passwords do not match!";
-            } else {
-                $db = new DB();
-                $user_exists = $db->checkUserExists($username, $email);
-                if ($user_exists) {
-                    $error = "Username or Email already exists!";
-                } else {
-                    $db->registerUser($username, $email, password_hash($password, PASSWORD_BCRYPT),"editor");
-                    appTemplate::redirect(appTemplate::getBaseUrl() . "/login");
-                }
-            }
-        }
-
-        $view = new appTemplate("register_editor.phtml");
-        $view->set("error", $error);
-
-        return appTemplate::loadLayout(array("content" => $view->output(), "title" => "Register"));
-    }
-
-    public static function registerAdminAction($args = array())
-    {
-        $error = "";
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $username = App::test_input($_POST['username']);
-            $email = App::test_input($_POST['email']);
-            $password = App::test_input($_POST['password']);
-            $confirm_password = App::test_input($_POST['confirm_password']);
-
-            if ($password !== $confirm_password) {
-                $error = "Passwords do not match!";
-            } else {
-                $db = new DB();
-                $user_exists = $db->checkUserExists($username, $email);
-                if ($user_exists) {
-                    $error = "Username or Email already exists!";
-                } else {
-                    $db->registerUser($username, $email, password_hash($password, PASSWORD_BCRYPT),"admin");
-                    appTemplate::redirect(appTemplate::getBaseUrl() . "/login");
-                }
-            }
-        }
-
-        $view = new appTemplate("register_admin.phtml");
-        $view->set("error", $error);
-
-        return appTemplate::loadLayout(array("content" => $view->output(), "title" => "Register"));
-    }
-    
-    
-    public static function logoutAction($args = array())
-    {
+    public static function logoutAction($args = array()) {
         session_destroy();
+        appTemplate::clearSessionState();
         appTemplate::redirect(appTemplate::getBaseUrl() . "/login");
     }
-    
-    public static function noRoute($args)
-    {
-        self::checkAuthentication();
-        $view = new appTemplate("404.phtml");
-        $view->set("title", "404 - No page found");
-        return appTemplate::loadLayout(array("content" => $view->output(), "title" => "404 - No page found"));
-    }
-    
-    private static function checkAuthentication()
-    {
+
+    private static function checkAuthentication() {
         if (!isset($_SESSION['logged_in']) || !$_SESSION['logged_in']) {
             appTemplate::redirect(appTemplate::getBaseUrl() . "/login");
         }
